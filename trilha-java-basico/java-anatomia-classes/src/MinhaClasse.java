fb8db9af27b199c9e511ac68fa964ba756717fef
eduardo-nascimento-de-almeida <eduardo.nalmeida3@gmail.com>
2023-01-09 20:15:11 -0300

    entendendo variáveis e métodos

--- trilha-java-basico/java-anatomia-classes/src/MinhaClasse.java
+++ trilha-java-basico/java-anatomia-classes/src/MinhaClasse.java
@@ -1,7 +1,16 @@
 public class MinhaClasse {
     
 public static void main (String [] args) {
-    System.out.print( "Hello, world!" );
+
+    String primeiroNome = "Eduardo";
+    String segundoNome = "Almeida";
+
+    String nomeCompleto = nomeCompleto (primeiroNome, segundoNome);
+    System.out.println(nomeCompleto);
+}
+
+public static String nomeCompleto (String primeiroNome, String segundoNome) {
+    return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
 }
 
 }
