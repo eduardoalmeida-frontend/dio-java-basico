ef216da7b22323ff8db756b759bececb6dac4d6e
eduardo-nascimento-de-almeida <eduardo.nalmeida3@gmail.com>
2023-01-12 20:46:11 -0300

    concatenacao de strings com numeros

--- trilha-java-basico/java-anatomia-classes/src/edu/eduardo/estudos/operadores/Operadores.java
+++ trilha-java-basico/java-anatomia-classes/src/edu/eduardo/estudos/operadores/Operadores.java
@@ -2,8 +2,28 @@ package edu.eduardo.estudos.operadores;
 
 public class Operadores {
     public static void main(String[] args) {
-        String nomeCompleto = "LINGUAGEM" + "JAVA";
-        
-        System.out.println(nomeCompleto);
+        String concatenacao = "?";
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1 + 1 + 1 + "1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1 + "1" + 1 + 1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1 + "1" + 1 + "1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1" + 1 + 1 + 1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1" + (1 + 1 + 1);
+
+        System.out.println(concatenacao);
     }
 }
