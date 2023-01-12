9cda8b7e095ae81f448455e2331cf0486b3041a2
eduardo-nascimento-de-almeida <eduardo.nalmeida3@gmail.com>
2023-01-11 22:13:44 -0300

    entendendo o recurso casting e declaracao de variaveis

--- trilha-java-basico/java-anatomia-classes/src/edu/eduardo/estudos/tipos_variaveis/TiposVariaveis.java
+++ trilha-java-basico/java-anatomia-classes/src/edu/eduardo/estudos/tipos_variaveis/TiposVariaveis.java
@@ -2,6 +2,10 @@ package edu.eduardo.estudos.tipos_variaveis;
 
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinino = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
     }
 }
