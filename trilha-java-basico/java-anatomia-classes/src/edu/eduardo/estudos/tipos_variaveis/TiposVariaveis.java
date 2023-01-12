37c9818f9cf942790d88417ed54baba862249603
eduardo-nascimento-de-almeida <eduardo.nalmeida3@gmail.com>
2023-01-11 21:35:01 -0300

    criando a pasta para exemplo de tipos e variaveis

---
+++ trilha-java-basico/java-anatomia-classes/src/edu/eduardo/estudos/tipos_variaveis/TiposVariaveis.java
@@ -0,0 +1,7 @@
+package edu.eduardo.estudos.tipos_variaveis;
+
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
