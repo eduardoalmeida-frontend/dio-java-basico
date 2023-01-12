528df922d117e2af5a87e177eca24d8bc7bce068
eduardo-nascimento-de-almeida <eduardo.nalmeida3@gmail.com>
2023-01-11 22:24:00 -0300

    convenção das constantes (final e caixa alta)

--- trilha-java-basico/java-anatomia-classes/src/edu/eduardo/estudos/tipos_variaveis/TiposVariaveis.java
+++ trilha-java-basico/java-anatomia-classes/src/edu/eduardo/estudos/tipos_variaveis/TiposVariaveis.java
@@ -7,5 +7,13 @@ public class TiposVariaveis {
         short numeroCurto = 1;
         int numeroNormal = numeroCurto;
         short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+        
+        numero = 10;
+
+        System.out.print(numero);
+
+        final double VALOR_DE_PI = 3.14;
     }
 }
