24baf995bc44c37f95671ea76fb39d029e8418b4
eduardo-nascimento-de-almeida <eduardo.nalmeida3@gmail.com>
2023-01-23 22:24:17 -0300

    entendendo os operadores unarios

--- trilha-java-basico/java-anatomia-classes/src/edu/eduardo/estudos/operadores/Operadores.java
+++ trilha-java-basico/java-anatomia-classes/src/edu/eduardo/estudos/operadores/Operadores.java
@@ -2,28 +2,16 @@ package edu.eduardo.estudos.operadores;
 
 public class Operadores {
     public static void main(String[] args) {
-        String concatenacao = "?";
+        int numero = 5;
 
-        System.out.println(concatenacao);
+        // Converte numero para negativo;
+        numero = - numero;
 
-        concatenacao = 1 + 1 + 1 + "1";
+        System.out.println(numero);
 
-        System.out.println(concatenacao);
+        // Converte numero para positivo;
+        numero = numero * -1;
 
-        concatenacao = 1 + "1" + 1 + 1;
-
-        System.out.println(concatenacao);
-
-        concatenacao = 1 + "1" + 1 + "1";
-
-        System.out.println(concatenacao);
-
-        concatenacao = "1" + 1 + 1 + 1;
-
-        System.out.println(concatenacao);
-
-        concatenacao = "1" + (1 + 1 + 1);
-
-        System.out.println(concatenacao);
+        System.out.println(numero);
     }
 }
