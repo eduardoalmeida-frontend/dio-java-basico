1410516aaad338ec3ed450d04116b5768754589c
eduardo-nascimento-de-almeida <eduardo.nalmeida3@gmail.com>
2023-01-23 22:34:40 -0300

    decremento e incremento antes e depois da variavel

--- trilha-java-basico/java-anatomia-classes/src/edu/eduardo/estudos/operadores/Operadores.java
+++ trilha-java-basico/java-anatomia-classes/src/edu/eduardo/estudos/operadores/Operadores.java
@@ -4,13 +4,11 @@ public class Operadores {
     public static void main(String[] args) {
         int numero = 5;
 
-        // Converte numero para negativo;
-        numero = - numero;
+        //x repeticao
+        ;
+        // numero = numero + 1;
 
-        System.out.println(numero);
-
-        // Converte numero para positivo;
-        numero = numero * -1;
+        System.out.println(numero --);
 
         System.out.println(numero);
     }
