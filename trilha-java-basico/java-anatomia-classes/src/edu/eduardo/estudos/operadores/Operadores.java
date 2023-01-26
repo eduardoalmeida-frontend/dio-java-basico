3b43942abfe98eef1229a415098da1a4a0f12dc6
eduardo-nascimento-de-almeida <eduardo.nalmeida3@gmail.com>
2023-01-25 22:38:26 -0300

    entendendo o operador ternario

--- trilha-java-basico/java-anatomia-classes/src/edu/eduardo/estudos/operadores/Operadores.java
+++ trilha-java-basico/java-anatomia-classes/src/edu/eduardo/estudos/operadores/Operadores.java
@@ -2,10 +2,12 @@ package edu.eduardo.estudos.operadores;
 
 public class Operadores {
     public static void main(String[] args) {
-        boolean variavel = true;
+        int a, b;
+        a = 6;
+        b = 6;
 
-        variavel = !variavel;
-        
-        System.out.println(variavel);
+        String resultado = a == b ? "verdadeiro" : "falso";
+
+        System.out.println(resultado);
     }
 }
