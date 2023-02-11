b89b2ec73f853f91ecdcff096de0977bbe90abbf
eduardo-nascimento-de-almeida <eduardo.nalmeida3@gmail.com>
2023-02-10 21:58:32 -0300

    operador E (&&) e operador OU (||)

--- trilha-java-basico/java-anatomia-classes/src/edu/eduardo/estudos/operadores/Operadores.java
+++ trilha-java-basico/java-anatomia-classes/src/edu/eduardo/estudos/operadores/Operadores.java
@@ -2,28 +2,18 @@ package edu.eduardo.estudos.operadores;
 
 public class Operadores {
     public static void main(String[] args) {
-        String nomeUm = "GLEYSON";
-        String nomeDois = new String("GLEYSON");
+        boolean condicao1 = true;
 
-        System.out.println(nomeUm.equals(nomeDois));
+        boolean condicao2 = true;
 
-        int numero1 = 1;
-        int numero2 = 2;
-
-        boolean simNao = numero1 == numero2;
-
-        if(numero1 == numero2){ //true
-            System.out.println("a nossa condição é verdadeira");
+        if(condicao1 && ( 7 > 4 )){
+            System.out.println("as duas condições são verdadeiras");
         }
 
-        System.out.println("numeroUm é igual a numeroDois? " + simNao);
+        if(condicao1 || condicao2){
+            System.out.println("uma das condições é verdadeira");
+        }
 
-        simNao = numero1 != numero2;
-
-        System.out.println("numeroUm é diferente a numeroDois? " + simNao);
-
-        simNao = numero1 > numero2;
-
-        System.out.println("numeroUm é maior que numeroDois? " + simNao);
+        System.out.println("fim");
     }
 }
