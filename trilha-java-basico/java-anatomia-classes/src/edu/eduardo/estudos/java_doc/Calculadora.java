effcd06818faee3aefee3482bd346c0d792ebb9d
eduardo-nascimento-de-almeida <eduardo.nalmeida3@gmail.com>
2023-02-24 23:05:45 -0300

    utilização de tags para documentação do projeto

---
+++ trilha-java-basico/java-anatomia-classes/src/edu/eduardo/estudos/java_doc/Calculadora.java
@@ -0,0 +1,20 @@
+package edu.eduardo.estudos.java_doc;
+
+/**
+ * @author Eduardo Almeid
+ * @version 1.0
+ * @since 24/02/2023
+ */
+
+public class Calculadora {
+    /**
+     * Este método é utilizado para somar dois números inteiros
+     * @param numeroUm este é o primeiro parâmetro do método
+     * @param numeroDois este é o segundo parâmetro do método
+     * @return int o resultado deste método é a soma dos dois números.
+     */
+    public int somar(int numeroUm, int numeroDois) {
+        return numeroUm + numeroDois;
+    }    
+
+}
