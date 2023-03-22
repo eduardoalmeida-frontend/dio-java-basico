ee6126d06ce1e94d677f718d38286c7c4784943b
eduardo_almeida <eduardo.nalmeida3@gmail.com>
2023-03-22 07:46:37 -0300

    estrutura de repetição while com teste lógico

---
+++ trilha-java-basico/java-anatomia-classes/src/edu/eduardo/estudos/estruturas_de_repeticao_e_arrays/Ex2_Nota.java
@@ -0,0 +1,21 @@
+package edu.eduardo.estudos.estruturas_de_repeticao_e_arrays;
+
+import java.util.Scanner;
+
+public class Ex2_Nota {
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+
+        int nota;
+
+        System.out.print("Nota: ");
+        nota = scan.nextInt();
+
+        while(nota < 0 || nota > 10) {
+            System.out.println("Nota inválida! Digite novamente: ");
+            nota = scan.nextInt();
+        }
+
+    }
+}
