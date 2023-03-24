b107c8709b64879c69ac7d1f39cc086987e1c331
eduardo_almeida <eduardo.nalmeida3@gmail.com>
2023-03-23 23:22:07 -0300

    exercicio: fatorial usando o laço for

---
+++ trilha-java-basico/java-anatomia-classes/src/edu/eduardo/estudos/estruturas_de_repeticao_e_arrays/Ex6_Fatorial.java
@@ -0,0 +1,19 @@
+package edu.eduardo.estudos.estruturas_de_repeticao_e_arrays;
+
+import java.util.Scanner;
+
+public class Ex6_Fatorial {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int multiplicacao = 1;
+
+        System.out.print("Fatorial: ");
+        int fatorial = scan.nextInt();
+
+        for (int i = fatorial; i >= 1; i--) {
+            multiplicacao *= i;
+        }
+
+        System.out.println(fatorial + "! = " + multiplicacao);
+    }
+}
