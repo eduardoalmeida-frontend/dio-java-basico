68b57ac489ccc73ea434d47d6fae7ccf5612fa2d
eduardoalmeida-frontend <eduardo.nalmeida3@gmail.com>
2023-03-26 13:51:55 -0300

    exercicio: imprimindo um vetor na ordem inversa usando o laço for

---
+++ trilha-java-basico/java-anatomia-classes/src/edu/eduardo/estudos/estruturas_de_repeticao_e_arrays/Ex1_OrdemInversa.java
@@ -0,0 +1,21 @@
+package edu.eduardo.estudos.estruturas_de_repeticao_e_arrays;
+
+public class Ex1_OrdemInversa {
+    public static void main(String[] args) {
+        
+        int[] vetor = {-5, -6, 15, 50, 8, 4};
+        int count = 0;
+
+        System.out.print("Vetor (ordenado): ");
+        while (count < (vetor.length)) {
+            System.out.print(vetor[count] + ", ");
+            count++;
+        }
+
+        System.out.print("\nVetor (ordem inversa): ");
+        for (int i = vetor.length - 1; i >= 0; i--) {
+            System.out.print(vetor[i] + ", ");
+        }
+
+    }
+}
