035822975b87cb7905ab109fa0633e4a22d447d9
eduardoalmeida-frontend <eduardo.nalmeida3@gmail.com>
2023-03-26 15:19:52 -0300

    exercicio: matriz unidimensional usando foreachs aninhados

---
+++ trilha-java-basico/java-anatomia-classes/src/edu/eduardo/estudos/arrays/Ex4_ArrayMultidimensional.java
@@ -0,0 +1,24 @@
+package edu.eduardo.estudos.arrays;
+
+import java.util.Random;
+
+public class Ex4_ArrayMultidimensional {
+    public static void main(String[] args) {
+        Random randon = new Random();
+        int[][] M = new int[4][4];
+    
+        for (int i = 0; i < M.length; i++) {
+            for (int j = 0; j < M[i].length; j++) {
+                M[i][j] = randon.nextInt(9);
+            }
+        }
+
+        System.out.println("Matriz: ");
+        for (int[] linha : M) {
+            for (int coluna : linha) {
+                System.out.print(coluna + " ");
+            }
+            System.out.println();
+        }
+    }
+}
