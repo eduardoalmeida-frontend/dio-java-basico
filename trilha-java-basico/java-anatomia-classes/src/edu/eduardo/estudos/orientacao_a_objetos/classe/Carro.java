994421918f97ef5cc7a3789dbbe46bbae4454909
eduardo_almeida <eduardo.nalmeida3@gmail.com>
2023-04-02 18:43:02 -0300

    exercício: criação do Construtor, sobrecarga dele, getters e setters e método de regra de negócio

--- trilha-java-basico/java-anatomia-classes/src/edu/eduardo/estudos/orientacao_a_objetos/classe/Carro.java
+++ trilha-java-basico/java-anatomia-classes/src/edu/eduardo/estudos/orientacao_a_objetos/classe/Carro.java
@@ -1,8 +1,45 @@
 package edu.eduardo.estudos.orientacao_a_objetos.classe;
 
 class Carro {
-    
+
     String cor;
     String modelo;
     int capacidadeTanque;
+
+    Carro() {
+    }
+
+    Carro(String cor, String modelo, int capacidadeTanque) {
+        this.cor = cor;
+        this.modelo = modelo;
+        this.capacidadeTanque = capacidadeTanque;
+    }
+
+    String getCor() {
+        return cor;
+    }
+
+    void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    String getModelo() {
+        return modelo;
+    }
+
+    void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    int getCapacidadeTanque() {
+        return capacidadeTanque;
+    }
+
+    void setCapacidadeTanque(int capacidadeTanque) {
+        this.capacidadeTanque = capacidadeTanque;
+    }
+
+    double totalValorTanque(double valorCombustivel) {
+        return capacidadeTanque * valorCombustivel;
+    }
 }
